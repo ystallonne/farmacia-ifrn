352c87f73692df9bfcec123ce4493218b8db2dc4
edmilsonbcampos <edmilsonbcampos@8754ec8e-f983-11de-bfc1-19433896a63b>
2010-02-24 17:23:24 +0000

    Criação do projeto SIDAF, SIDAF.BLL, SIDAF.DAl e SIDAF.Model no Visual Studio e adicionado algumas classes exemplo

---
+++ trunk/codigo/SIDAF/SIDAF.DAL/Banco.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SIDAF.DAL
+{
+    public class Banco
+    {
+        private static string stringDeConexao = "";
+
+        private static SqlConnection conexao;
+
+        public static void AbrirConexao()
+        {
+            conexao = new SqlConnection(stringDeConexao);
+            conexao.Open();
+        }
+
+        public static void FecharConexao()
+        {
+            if (conexao != null && conexao.State == ConnectionState.Open)
+                conexao.Close();
+        }
+
+        public static SqlConnection Conexão
+        {
+            get { return conexao; }
+        }
+    }
+}
