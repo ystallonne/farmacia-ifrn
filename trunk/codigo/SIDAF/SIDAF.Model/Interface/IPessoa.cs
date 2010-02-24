5de0339bed2af47b6380f2275e1df6f162e0bed7
edmilsonbcampos <edmilsonbcampos@8754ec8e-f983-11de-bfc1-19433896a63b>
2010-02-24 17:39:06 +0000

    Criação do Diagrama de Clases no Visual Studio (início)

---
+++ trunk/codigo/SIDAF/SIDAF.Model/Interface/IPessoa.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SIDAF.Model.Interface
+{
+    public interface IPessoa
+    {
+        string Nome { get; set; }
+        int? Oid { get; set; }
+        string Telefone { get; set; }
+    }
+}
