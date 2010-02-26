10ed40dd4558f565fd399fb43a5cb4b12d85f1d5
helder.ifrn <helder.ifrn@8754ec8e-f983-11de-bfc1-19433896a63b>
2010-02-26 20:17:45 +0000

    Criadas as classes: Venda.cs, Estorno.cs, Saida.cs, Entrada.cs e Caixa.cs. Além disso, as classes sealed para cada uma das classes acima criadas e suas interfaces.

---
+++ trunk/codigo/SIDAF/SIDAF.Model/Interface/ICaixa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SIDAF.Model
+{
+    interface ICaixa
+    {
+        DateTime DataAbertura { get; set; }
+        DateTime DataFechamento { get; set; }
+        int? Oid { get; set; }
+    }
+}
---
+++ trunk/codigo/SIDAF/SIDAF.Model/Venda.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SIDAF.Model.Interface;
+
+
+
+namespace SIDAF.Model
+{
+    class Venda : IVenda, SIDAF.Model.IVenda
+    {
+        #region Fields
+        private Nullable<int> oid;
+
+        public Nullable<int> Oid
+        {
+            get { return oid; }
+            set { oid = value; }
+        }
+
+        private DateTime dataVenda;
+
+        public DateTime DataVenda
+        {
+            get { return dataVenda; }
+            set { dataVenda = value; }
+        }
+        private Funcionario func;
+        private int tipoVenda;
+        private List<ItemVenda> itens;
+        private Empresa emp;
+        
+        #endregion Fields
+    }
+}
