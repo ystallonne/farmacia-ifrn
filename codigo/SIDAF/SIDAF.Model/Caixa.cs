ceba10345339fd0038bcf98aeba7ecf9f76c21ca
edmilsonbcampos <edmilsonbcampos@8754ec8e-f983-11de-bfc1-19433896a63b>
2010-02-26 23:52:55 +0000

    Criação dos get e set para o field funcionario, venda, entrada, saida e estorno em Caixa.cs

--- codigo/SIDAF/SIDAF.Model/Caixa.cs
+++ codigo/SIDAF/SIDAF.Model/Caixa.cs
@@ -30,10 +30,40 @@ namespace SIDAF.Model
             set { dataFechamento = value; }
         }
         private Funcionario funcionario;
+
+        public Funcionario Funcionario
+        {
+            get { return funcionario; }
+            set { funcionario = value; }
+        }
         private Venda venda;
+
+        public Venda Venda
+        {
+            get { return venda; }
+            set { venda = value; }
+        }
         private Entrada entrada;
+
+        public Entrada Entrada
+        {
+            get { return entrada; }
+            set { entrada = value; }
+        }
         private Saida saida;
+
+        public Saida Saida
+        {
+            get { return saida; }
+            set { saida = value; }
+        }
         private Estorno estorno;
+
+        public Estorno Estorno
+        {
+            get { return estorno; }
+            set { estorno = value; }
+        }
 
         #endregion
     }
