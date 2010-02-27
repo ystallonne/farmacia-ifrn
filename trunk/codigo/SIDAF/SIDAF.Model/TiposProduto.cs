4ac52e25434f8c31597b4835f71cbb68dabda639
edmilsonbcampos <edmilsonbcampos@8754ec8e-f983-11de-bfc1-19433896a63b>
2010-02-27 00:40:53 +0000

    Criação da classes TiposProduto.cs

---
+++ trunk/codigo/SIDAF/SIDAF.Model/TiposProduto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIDAF.Model
+{
+    public sealed class TiposProduto: List<TipoProduto>
+    {
+    }
+}
