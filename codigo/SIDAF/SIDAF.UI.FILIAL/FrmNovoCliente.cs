19ed9a58ff369dfd8fcd0d2bf03916203ca3f781
kairon.ramon <kairon.ramon@8754ec8e-f983-11de-bfc1-19433896a63b>
2010-03-04 20:47:13 +0000

    Criado a tela de Cadastro de cliente

---
+++ codigo/SIDAF/SIDAF.UI.FILIAL/FrmNovoCliente.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SIDAF.UI.FILIAL
+{
+    public partial class FrmNovoCliente : Form
+    {
+        public FrmNovoCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label26_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Contatos_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
