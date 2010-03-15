1412895c7e6607fc7ca448f2618b41e9e9c854c5
amandafederal <amandafederal@8754ec8e-f983-11de-bfc1-19433896a63b>
2010-03-15 21:05:09 +0000

    Procedures de Busca de Produto por Nome, Código e Princípio Ativo (obs. add princAtivo na tabela de Produto)

--- codigo/SIDAF/SIDAF.UI.FILIAL/Gerencia/frmBuscaProduto.Designer.cs
+++ codigo/SIDAF/SIDAF.UI.FILIAL/Gerencia/frmBuscaProduto.Designer.cs
@@ -39,9 +39,12 @@
             this.IdEspecificacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.IdTipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.IdProduto = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Especificacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Produto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrincAtivo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Especificacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Tipo = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Qtd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Preco = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -49,7 +52,7 @@
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(308, 38);
+            this.btnBuscar.Location = new System.Drawing.Point(347, 38);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(75, 23);
             this.btnBuscar.TabIndex = 0;
@@ -61,17 +64,17 @@
             // 
             this.comboBoxFiltro.FormattingEnabled = true;
             this.comboBoxFiltro.Items.AddRange(new object[] {
-            "Especificação",
-            "Produto",
-            "Tipo"});
+            "Código do Produto",
+            "Nome do Produto",
+            "Princípio Ativo"});
             this.comboBoxFiltro.Location = new System.Drawing.Point(18, 40);
             this.comboBoxFiltro.Name = "comboBoxFiltro";
-            this.comboBoxFiltro.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxFiltro.Size = new System.Drawing.Size(160, 21);
             this.comboBoxFiltro.TabIndex = 1;
             // 
             // txtBusca
             // 
-            this.txtBusca.Location = new System.Drawing.Point(145, 40);
+            this.txtBusca.Location = new System.Drawing.Point(184, 40);
             this.txtBusca.Name = "txtBusca";
             this.txtBusca.Size = new System.Drawing.Size(157, 20);
             this.txtBusca.TabIndex = 2;
@@ -86,7 +89,7 @@
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(451, 78);
+            this.groupBox1.Size = new System.Drawing.Size(884, 78);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Parâmetros de Pesquisa";
@@ -94,7 +97,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(142, 23);
+            this.label2.Location = new System.Drawing.Point(181, 23);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 13);
             this.label2.TabIndex = 4;
@@ -115,7 +118,7 @@
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox2.Location = new System.Drawing.Point(0, 78);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(451, 301);
+            this.groupBox2.Size = new System.Drawing.Size(884, 480);
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Resultados da Busca";
@@ -128,13 +131,16 @@
             this.IdEspecificacao,
             this.IdTipo,
             this.IdProduto,
-            this.Especificacao,
             this.Produto,
-            this.Tipo});
+            this.PrincAtivo,
+            this.Especificacao,
+            this.Tipo,
+            this.Qtd,
+            this.Preco});
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(3, 16);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(445, 282);
+            this.dataGridView1.Size = new System.Drawing.Size(878, 461);
             this.dataGridView1.TabIndex = 0;
             // 
             // IdEspecificacao
@@ -151,30 +157,46 @@
             // 
             // IdProduto
             // 
-            this.IdProduto.HeaderText = "IdProduto";
+            this.IdProduto.HeaderText = "Código";
             this.IdProduto.Name = "IdProduto";
-            this.IdProduto.Visible = false;
-            // 
-            // Especificacao
-            // 
-            this.Especificacao.HeaderText = "Especificação";
-            this.Especificacao.Name = "Especificacao";
             // 
             // Produto
             // 
             this.Produto.HeaderText = "Produto";
             this.Produto.Name = "Produto";
+            this.Produto.Width = 150;
+            // 
+            // PrincAtivo
+            // 
+            this.PrincAtivo.HeaderText = "Princípio Ativo";
+            this.PrincAtivo.Name = "PrincAtivo";
+            // 
+            // Especificacao
+            // 
+            this.Especificacao.HeaderText = "Especificação";
+            this.Especificacao.Name = "Especificacao";
+            this.Especificacao.Width = 150;
             // 
             // Tipo
             // 
             this.Tipo.HeaderText = "Tipo";
             this.Tipo.Name = "Tipo";
             // 
+            // Qtd
+            // 
+            this.Qtd.HeaderText = "Estoque";
+            this.Qtd.Name = "Qtd";
+            // 
+            // Preco
+            // 
+            this.Preco.HeaderText = "Preço";
+            this.Preco.Name = "Preco";
+            // 
             // frmBuscaProduto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(451, 379);
+            this.ClientSize = new System.Drawing.Size(884, 558);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "frmBuscaProduto";
@@ -201,8 +223,11 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn IdEspecificacao;
         private System.Windows.Forms.DataGridViewTextBoxColumn IdTipo;
         private System.Windows.Forms.DataGridViewTextBoxColumn IdProduto;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Especificacao;
         private System.Windows.Forms.DataGridViewTextBoxColumn Produto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PrincAtivo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Especificacao;
         private System.Windows.Forms.DataGridViewComboBoxColumn Tipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Qtd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Preco;
     }
 }
