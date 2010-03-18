f274b22b3a0939c58b7507ca2100930680e73baf
kairon.ramon <kairon.ramon@8754ec8e-f983-11de-bfc1-19433896a63b>
2010-03-18 15:54:46 +0000

    Alterações nos procedures de cliente e funcionario

--- banco/Projeto Scripts/SIDAFScripts/SIDAFScripts/PRO_atualizarCLIENTE.sql
+++ banco/Projeto Scripts/SIDAFScripts/SIDAFScripts/PRO_atualizarCLIENTE.sql
@@ -7,23 +7,60 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE atualizarCLIENTE
+ALTER PROCEDURE [dbo].[atualizarCLIENTE]
+	
+	--DADOS DE CLIENTE
 	@IDCliente int,
+	@IDUsuario int,
+	@IDEndereco int,
+	@nome varchar(150),
+	@dataNascimento datetime,
 	@cpf char(11),
 	@rg int,
-	@orgaoEmissor varchar(50),
+	@orgaoEmissorRg varchar(10),
 	@naturalidade varchar(50),
 	@nacionalidade varchar(50),
+	@telefoneFixo nchar(10),
+	@celular nchar(10),
+	@email varchar(50),
 	@dataCadastro date,
-	@IDUsuario int
+	@dataUltimaAlteracao date,
+	-- DADOS DE USUARIO
+	@login varchar(50),
+	@senha varchar(1000),
+	-- DADOS DE ENDEREÇO
+	@IDCidade int,
+	@cep int,
+	@logradouro varchar(150),
+	@numero int,
+	@complemento varchar(20),
+	@bairro varchar(100)
 AS
-	UPDATE [SIDAF].[dbo].[Cliente]
-	SET [cpf] = @cpf
-	   ,[rg] = @rg
-       ,[orgaoEmissorRg] = @orgaoEmissor
-       ,[naturalidade] = @naturalidade
-       ,[nacionalidade] = @nacionalidade
-       ,[dataCadastro] = @dataCadastro
-       ,[IDUsuario] = @IDUsuario
-	WHERE IDCliente = @IDCliente
+	UPDATE Cliente
+	SET	 nome = @nome
+		,dataNascimento = @dataNascimento
+		,cpf = @cpf		
+		,rg = @rg
+		,orgaoEmissorRg = @orgaoEmissor
+		,naturalidade = @naturalidade
+		,nacionalidade = @nacionalidade
+		,telefoneFixo = @telefoneFixo
+		,celular = @celular
+		,email = @email
+		,dataUltimaAlteracao = @dataUltimaAlteracao
+		WHERE IDCliente = @IDCliente
+		UPDATE Usuario
+			SET	 login = @login
+				,senha = dbo.CriptografaSenha(@senha)
+				WHERE IDUsuario = @IDUsuario
+				
+		UPDATE Endereco
+			SET  cep = @cep
+				,logradouro = @logradouro
+				,numero = @numero
+				,complemento = @complemento
+				,bairro = @bairro
+				,IDCidade = @IDCidade
+				WHERE IDEndereco = @IDEndereco
 GO
+		
