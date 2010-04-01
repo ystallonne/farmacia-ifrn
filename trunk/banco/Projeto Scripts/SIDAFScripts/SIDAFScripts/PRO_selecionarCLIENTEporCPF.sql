c70cd0f64ad02663520cb9f15730e621450f8b4f
edmilsonbcampos <edmilsonbcampos@8754ec8e-f983-11de-bfc1-19433896a63b>
2010-04-01 11:28:55 +0000

    Alteração nas procedures

--- trunk/banco/Projeto Scripts/SIDAFScripts/SIDAFScripts/PRO_selecionarCLIENTEporCPF.sql
+++ trunk/banco/Projeto Scripts/SIDAFScripts/SIDAFScripts/PRO_selecionarCLIENTEporCPF.sql
@@ -11,5 +11,9 @@ CREATE PROCEDURE selecionarCLIENTEporCPF
 	@cpf char
 AS
 	SELECT * FROM [SIDAF].[dbo].[Cliente]
+	INNER JOIN Usuario
+	on Usuario.IDLogin = Cliente.IDUsuario
+	LEFT JOIN Endereco
+	on Endereco.IDEndereco = Cliente.IDEndereco
 	WHERE cpf = @cpf
 GO
