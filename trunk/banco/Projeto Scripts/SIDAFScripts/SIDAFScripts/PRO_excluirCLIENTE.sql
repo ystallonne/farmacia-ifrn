7bdee6b2beb30f5db50d29d3fb85da3ed47cbcc1
edmilsonbcampos <edmilsonbcampos@8754ec8e-f983-11de-bfc1-19433896a63b>
2010-04-01 11:29:38 +0000

    Alteração nas procedures

--- trunk/banco/Projeto Scripts/SIDAFScripts/SIDAFScripts/PRO_excluirCLIENTE.sql
+++ trunk/banco/Projeto Scripts/SIDAFScripts/SIDAFScripts/PRO_excluirCLIENTE.sql
@@ -1,6 +1,11 @@
 USE [SIDAF]
 GO
 
+<<<<<<< .mine
+CREATE TRIGGER T_RemoverEndereco
+ON Cliente
+FOR DELETE
+=======
 SET ANSI_NULLS ON
 GO
 
@@ -11,7 +16,21 @@ ALTER PROCEDURE [dbo].[excluirCLIENTE]
 	@IDCliente int,
 	@IDUsuario int,
 	@IDEndereco int
+>>>>>>> .r140
 AS
+<<<<<<< .mine
+  BEGIN
+    DELETE FROM Endereco 
+    FROM deleted
+    WHERE Endereco.IDEndereco = deleted.IDEndereco
+    IF @@ERROR <> 0
+    BEGIN
+      ROLLBACK TRANSACTION
+      RAISERROR('Erro de Processamento – Chame o Administrador', 16,1)
+      RETURN
+    END
+  END
+=======
 	DELETE FROM Endereco
 	WHERE IDEndereco = @IDEndereco
 	
@@ -22,3 +41,4 @@ AS
 	WHERE IDCliente = @IDCliente
 GO
 
+>>>>>>> .r140
