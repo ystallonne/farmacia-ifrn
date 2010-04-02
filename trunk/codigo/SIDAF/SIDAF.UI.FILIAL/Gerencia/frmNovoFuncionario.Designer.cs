1b3b86fa59313f890deefbc0f703c63057c66a48
kairon.ramon <kairon.ramon@8754ec8e-f983-11de-bfc1-19433896a63b>
2010-04-02 19:54:02 +0000

    add botoes dos forms

--- trunk/codigo/SIDAF/SIDAF.UI.FILIAL/Gerencia/frmNovoFuncionario.Designer.cs
+++ trunk/codigo/SIDAF/SIDAF.UI.FILIAL/Gerencia/frmNovoFuncionario.Designer.cs
@@ -29,19 +29,24 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtNome = new System.Windows.Forms.TextBox();
-            this.txtRG = new System.Windows.Forms.TextBox();
-            this.mtxtCPF = new System.Windows.Forms.MaskedTextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtOrgao = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txtNaturalidade = new System.Windows.Forms.TextBox();
+            this.cbCargo = new System.Windows.Forms.ComboBox();
+            this.label13 = new System.Windows.Forms.Label();
             this.txtNacionalidade = new System.Windows.Forms.TextBox();
+            this.txtNaturalidade = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtOrgao = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mtxtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.txtRG = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbUF = new System.Windows.Forms.ComboBox();
+            this.mtxtCEP = new System.Windows.Forms.MaskedTextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
             this.txtCidade = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
@@ -51,18 +56,15 @@
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
-            this.txtNumero = new System.Windows.Forms.TextBox();
-            this.mtxtCEP = new System.Windows.Forms.MaskedTextBox();
-            this.cbUF = new System.Windows.Forms.ComboBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.mtxtCelular = new System.Windows.Forms.MaskedTextBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.mtxtTelefone = new System.Windows.Forms.MaskedTextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.label16 = new System.Windows.Forms.Label();
             this.label18 = new System.Windows.Forms.Label();
-            this.mtxtTelefone = new System.Windows.Forms.MaskedTextBox();
-            this.mtxtCelular = new System.Windows.Forms.MaskedTextBox();
-            this.label19 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.cbCargo = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -91,54 +93,62 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Dados Pessoais";
             // 
-            // label1
+            // cbCargo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nome:";
+            this.cbCargo.FormattingEnabled = true;
+            this.cbCargo.Location = new System.Drawing.Point(64, 70);
+            this.cbCargo.Name = "cbCargo";
+            this.cbCargo.Size = new System.Drawing.Size(128, 21);
+            this.cbCargo.TabIndex = 14;
             // 
-            // label2
+            // label13
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(198, 74);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "CPF:";
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(20, 74);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(41, 13);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "Cargo: ";
             // 
-            // label3
+            // txtNacionalidade
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(326, 74);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(26, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "RG:";
+            this.txtNacionalidade.Location = new System.Drawing.Point(389, 110);
+            this.txtNacionalidade.Name = "txtNacionalidade";
+            this.txtNacionalidade.Size = new System.Drawing.Size(192, 20);
+            this.txtNacionalidade.TabIndex = 12;
             // 
-            // txtNome
+            // txtNaturalidade
             // 
-            this.txtNome.Location = new System.Drawing.Point(64, 35);
-            this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(514, 20);
-            this.txtNome.TabIndex = 3;
+            this.txtNaturalidade.Location = new System.Drawing.Point(96, 110);
+            this.txtNaturalidade.Name = "txtNaturalidade";
+            this.txtNaturalidade.Size = new System.Drawing.Size(192, 20);
+            this.txtNaturalidade.TabIndex = 11;
             // 
-            // txtRG
+            // label6
             // 
-            this.txtRG.Location = new System.Drawing.Point(358, 71);
-            this.txtRG.Name = "txtRG";
-            this.txtRG.Size = new System.Drawing.Size(77, 20);
-            this.txtRG.TabIndex = 5;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(305, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Nacionalidade:";
             // 
-            // mtxtCPF
+            // label5
             // 
-            this.mtxtCPF.Location = new System.Drawing.Point(234, 71);
-            this.mtxtCPF.Mask = "999.999.999-99";
-            this.mtxtCPF.Name = "mtxtCPF";
-            this.mtxtCPF.Size = new System.Drawing.Size(86, 20);
-            this.mtxtCPF.TabIndex = 6;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Naturalidade:";
+            // 
+            // txtOrgao
+            // 
+            this.txtOrgao.Location = new System.Drawing.Point(525, 71);
+            this.txtOrgao.Name = "txtOrgao";
+            this.txtOrgao.Size = new System.Drawing.Size(53, 20);
+            this.txtOrgao.TabIndex = 8;
+            this.txtOrgao.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label4
             // 
@@ -150,45 +160,54 @@
             this.label4.Text = "Orgão Emissor:";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // txtOrgao
+            // mtxtCPF
             // 
-            this.txtOrgao.Location = new System.Drawing.Point(525, 71);
-            this.txtOrgao.Name = "txtOrgao";
-            this.txtOrgao.Size = new System.Drawing.Size(53, 20);
-            this.txtOrgao.TabIndex = 8;
-            this.txtOrgao.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.mtxtCPF.Location = new System.Drawing.Point(234, 71);
+            this.mtxtCPF.Mask = "999.999.999-99";
+            this.mtxtCPF.Name = "mtxtCPF";
+            this.mtxtCPF.Size = new System.Drawing.Size(86, 20);
+            this.mtxtCPF.TabIndex = 6;
             // 
-            // label5
+            // txtRG
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(20, 113);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(70, 13);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Naturalidade:";
+            this.txtRG.Location = new System.Drawing.Point(358, 71);
+            this.txtRG.Name = "txtRG";
+            this.txtRG.Size = new System.Drawing.Size(77, 20);
+            this.txtRG.TabIndex = 5;
             // 
-            // label6
+            // txtNome
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(305, 113);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(78, 13);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "Nacionalidade:";
+            this.txtNome.Location = new System.Drawing.Point(64, 35);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(514, 20);
+            this.txtNome.TabIndex = 3;
             // 
-            // txtNaturalidade
+            // label3
             // 
-            this.txtNaturalidade.Location = new System.Drawing.Point(96, 110);
-            this.txtNaturalidade.Name = "txtNaturalidade";
-            this.txtNaturalidade.Size = new System.Drawing.Size(192, 20);
-            this.txtNaturalidade.TabIndex = 11;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(326, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "RG:";
             // 
-            // txtNacionalidade
+            // label2
             // 
-            this.txtNacionalidade.Location = new System.Drawing.Point(389, 110);
-            this.txtNacionalidade.Name = "txtNacionalidade";
-            this.txtNacionalidade.Size = new System.Drawing.Size(192, 20);
-            this.txtNacionalidade.TabIndex = 12;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CPF:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
             // 
             // groupBox2
             // 
@@ -210,6 +229,31 @@
             this.groupBox2.TabIndex = 13;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Endereço";
+            // 
+            // cbUF
+            // 
+            this.cbUF.FormattingEnabled = true;
+            this.cbUF.Location = new System.Drawing.Point(335, 114);
+            this.cbUF.Name = "cbUF";
+            this.cbUF.Size = new System.Drawing.Size(61, 21);
+            this.cbUF.TabIndex = 15;
+            this.cbUF.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // mtxtCEP
+            // 
+            this.mtxtCEP.Location = new System.Drawing.Point(509, 70);
+            this.mtxtCEP.Mask = "99999-999";
+            this.mtxtCEP.Name = "mtxtCEP";
+            this.mtxtCEP.Size = new System.Drawing.Size(68, 20);
+            this.mtxtCEP.TabIndex = 14;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(77, 74);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(98, 20);
+            this.txtNumero.TabIndex = 13;
+            this.txtNumero.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
             // 
             // txtCidade
             // 
@@ -287,31 +331,6 @@
             this.label12.TabIndex = 0;
             this.label12.Text = "Rua/Av.:";
             // 
-            // txtNumero
-            // 
-            this.txtNumero.Location = new System.Drawing.Point(77, 74);
-            this.txtNumero.Name = "txtNumero";
-            this.txtNumero.Size = new System.Drawing.Size(98, 20);
-            this.txtNumero.TabIndex = 13;
-            this.txtNumero.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
-            // 
-            // mtxtCEP
-            // 
-            this.mtxtCEP.Location = new System.Drawing.Point(509, 70);
-            this.mtxtCEP.Mask = "99999-999";
-            this.mtxtCEP.Name = "mtxtCEP";
-            this.mtxtCEP.Size = new System.Drawing.Size(68, 20);
-            this.mtxtCEP.TabIndex = 14;
-            // 
-            // cbUF
-            // 
-            this.cbUF.FormattingEnabled = true;
-            this.cbUF.Location = new System.Drawing.Point(335, 114);
-            this.cbUF.Name = "cbUF";
-            this.cbUF.Size = new System.Drawing.Size(61, 21);
-            this.cbUF.TabIndex = 15;
-            this.cbUF.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.mtxtCelular);
@@ -326,6 +345,31 @@
             this.groupBox3.TabIndex = 16;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Contato";
+            // 
+            // mtxtCelular
+            // 
+            this.mtxtCelular.Location = new System.Drawing.Point(238, 31);
+            this.mtxtCelular.Mask = "(99)9999-9999";
+            this.mtxtCelular.Name = "mtxtCelular";
+            this.mtxtCelular.Size = new System.Drawing.Size(84, 20);
+            this.mtxtCelular.TabIndex = 18;
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(190, 34);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(42, 13);
+            this.label19.TabIndex = 17;
+            this.label19.Text = "Celular:";
+            // 
+            // mtxtTelefone
+            // 
+            this.mtxtTelefone.Location = new System.Drawing.Point(75, 31);
+            this.mtxtTelefone.Mask = "(99)9999-9999";
+            this.mtxtTelefone.Name = "mtxtTelefone";
+            this.mtxtTelefone.Size = new System.Drawing.Size(84, 20);
+            this.mtxtTelefone.TabIndex = 16;
             // 
             // txtEmail
             // 
@@ -352,53 +396,31 @@
             this.label18.TabIndex = 0;
             this.label18.Text = "Telefone:";
             // 
-            // mtxtTelefone
+            // button1
             // 
-            this.mtxtTelefone.Location = new System.Drawing.Point(75, 31);
-            this.mtxtTelefone.Mask = "(99)9999-9999";
-            this.mtxtTelefone.Name = "mtxtTelefone";
-            this.mtxtTelefone.Size = new System.Drawing.Size(84, 20);
-            this.mtxtTelefone.TabIndex = 16;
+            this.button1.Location = new System.Drawing.Point(533, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Inserir";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // mtxtCelular
+            // button2
             // 
-            this.mtxtCelular.Location = new System.Drawing.Point(238, 31);
-            this.mtxtCelular.Mask = "(99)9999-9999";
-            this.mtxtCelular.Name = "mtxtCelular";
-            this.mtxtCelular.Size = new System.Drawing.Size(84, 20);
-            this.mtxtCelular.TabIndex = 18;
-            // 
-            // label19
-            // 
-            this.label19.AutoSize = true;
-            this.label19.Location = new System.Drawing.Point(190, 34);
-            this.label19.Name = "label19";
-            this.label19.Size = new System.Drawing.Size(42, 13);
-            this.label19.TabIndex = 17;
-            this.label19.Text = "Celular:";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(20, 74);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(41, 13);
-            this.label13.TabIndex = 13;
-            this.label13.Text = "Cargo: ";
-            // 
-            // cbCargo
-            // 
-            this.cbCargo.FormattingEnabled = true;
-            this.cbCargo.Location = new System.Drawing.Point(64, 70);
-            this.cbCargo.Name = "cbCargo";
-            this.cbCargo.Size = new System.Drawing.Size(128, 21);
-            this.cbCargo.TabIndex = 14;
+            this.button2.Location = new System.Drawing.Point(440, 421);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // frmRealizarPedido
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(620, 407);
+            this.ClientSize = new System.Drawing.Size(620, 456);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -451,5 +473,7 @@
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.MaskedTextBox mtxtCelular;
         private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
